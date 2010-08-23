da3f1b9dc8801becf638b67410eb3f99b5fe9f9c
Philipp Staender <philipp.staender@gmail.com>
2010-08-23 12:08:28 +0200

    _config with custom config stuff

--- _config.php
+++ _config.php
@@ -1,7 +1,10 @@
 <?php
-
-Director::addRules(100, array(
-	'newsletter-tool/$Action/$ID/$OtherID' => 'NewsletterHolder_Controller',
-));
-
+//do some config stuff here, if you like
+//otherwise delete / comment the following lines
+NewsletterHolder::$newsletterEmail = "admin@127.0.0.1";
+//use a custom body template for email, otherwise its used an default Sapphire template
+NewsletterHolder::$emailBodyTemplate = null;
+NewsletterHolder::$signupRequiredFields = array("Email");
+//transform pictures + links to absolue urls (better for newsletters)
+NewsletterCamaign::$makeRelativeToAbsoluteURLS = true;
 ?>
